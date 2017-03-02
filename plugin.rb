6736e6214a92114b46a34442f3fb6034c1fbe33b
umanhay <ptown727@yahoo.com>
2017-03-01 18:16:58 -0800

    Add plugin.rb file

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: Wootric for Discourse
+# about: A simplistic way to integrate Wootric with Discourse
+# version: 0.0.1
+# authors: Sam McGahan and Eileen Szeto
+# url: https://github.com/Riddlerat/discourse-wootric
